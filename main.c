76a67c6935e625f2369ad789f5625fdbc678df02
Assassinoooo <oioi220419@gmail.com>
2025-05-14 23:59:59 +0700

    main.c template code

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    int pilihan = 0;
+    float a,b;
+    puts("1. Addition");
+    puts("2. Subtraction");
+    puts("3. Multiplication");
+    puts("4. Division");
+    printf("Masukan Pilihan Anda: ");
+    scanf("%d", &pilihan);
+    printf("Masukan nilai a: ");
+    scanf("%f",&a);
+    printf("Masukan nilai b: ");
+    scanf("%f",&b);
+    switch(pilihan){
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        default: 
+
+            break;
+    }
+
+    return 0;
+}
